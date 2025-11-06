a321f59e2fe803ebb6c6abef9f39f0fcb47edfa1
vaibhavxmahajan <vaibhavcse226@gmail.com>
2025-11-06 13:48:31 +0530

    nextjs project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full">
+        Docu Chat
+      </main>
+    </div>
+  );
+}
